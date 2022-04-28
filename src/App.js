47c6c8bbf57fcb97c5977b59d5a414c26bcfd1a9
KelvinReverts <kelvinrev2@hotmail.com>
2022-04-27 22:34:01 -0300

    initial commit
    Signed-off-by: KelvinReverts <kelvinrev2@hotmail.com>

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './css/App.css';
+import Footer from './componentes/Footer/';
+import Header from './componentes/Header';
+import HomePage from './componentes/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <HomePage/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css'
+
+export default function Card(){
+    return(
+        <div className="card-container">
+            <p>componentes:</p>
+            <p>Facilita em manter partes menores funcionando corretamente</p>
+            <p>Você pode reutiliza-lis, ou seja, menos codigo para escrever.</p>
+        </div>
+    )
+}
